22e919c99702d6863381f807552292eb0643d546
kkimjuseong <rofodzl1@naver.com>
2024-04-08 09:29:29 +0900

    # 연산자

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println(10 + 5);
-        System.out.println("안녕?");
+//        System.out.println(10 + 5);
+//        System.out.println("안녕?");
+//
+//        int month = 5;
+//        int day = 5;
+//        String anni = "어린이날";
+//
+//
+//        System.out.println(month + "월 " + day + "일은 " + anni + "입니다.");
+//        System.out.printf("%d월 %d일은 %s입니다.\n", month, day ,anni);
+////        System.out.printf("%d월 %d일은 %s입니다.", month, day ,anni);
+//        // 이거야
 
-        int month = 5;
-        int day = 5;
-        String anni = "어린이날";
+        int x = 10, y = 20;
 
+        System.out.println(x > 10 && (y / 0 == 10)); //false
+        System.out.println(x > 10 || y < 30); //true
 
-        System.out.println(month + "월 " + day + "일은 " + anni + "입니다.");
-        System.out.printf("%d월 %d일은 %s입니다.\n", month, day ,anni);
-//        System.out.printf("%d월 %d일은 %s입니다.", month, day ,anni);
-        // 이거야
     }
 }
