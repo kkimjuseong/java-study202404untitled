caca86f3fe2058c0fc995e191608bdc0d1f514e5
kkimjuseong <rofodzl1@naver.com>
2024-04-11 09:41:44 +0900

    # MethodQuiz0 05 remove

--- src/day03/MethodQuiz01T.java
+++ src/day03/MethodQuiz01T.java
@@ -7,41 +7,61 @@ public class MethodQuiz01T {
     static String[] foods = {"떡볶이", "치킨", "파스타"};
     static String[] userNames = {"홍길동", "고길동"};
 
-    static void printFoods(){
+    static void printFoods() {
         System.out.println(Arrays.toString(foods));
     }
 
-    // 사이즈를 조절해서 새 배열을 만드는 함수 리팩토링
-    static String[] makeNewArray(int size){
+    // 사이즈를 조절해서 새 배열을 만드는 함수
+    static String[] makeNewArray(int size) {
         return new String[foods.length + size];
     }
 
-    // 기존 데이터를 복사하는 함수 리팩토링
-    static String[] copy(int size){
-        String[] newfood = makeNewArray(size);
-        for (int i = 0; i < foods.length; i++) {
-            newfood[i] = foods[i];
+    // 기존 데이터를 복사하는 함수
+    static String[] copy(int size) {
+        // size를 조절한 배열 생성
+        String[] temp = makeNewArray(size);
+
+        int loopCount = (size >= 0) ? foods.length : temp.length;
+
+        for (int i = 0; i < loopCount; i++) {
+            temp[i] = foods[i];
         }
-        return newfood;
+        return temp;
     }
 
-    static void push(String food){
-
-        String[] newfood = copy(1);
-
-        newfood[foods.length] = food;
-        foods = newfood;
+    // foods배열에 데이터를 끝에 추가하는 함수
+    static void push(String newFood) {
+        String[] temp = copy(1);
+        // 새 배열의 끝인덱스에 새 데이터 추가
+        temp[temp.length - 1] = newFood;
+        foods = temp;
     }
 
-    //foods 배열에서 특정 데이터의 인덱스를 반환
-    static int indexOf(String searchFood){
+    // foods배열에서 특정 데이터의 인덱스를 반환
+    static int indexOf(String searchFood) {
         for (int i = 0; i < foods.length; i++) {
-            if(searchFood.equals(foods[i])){
+            if (searchFood.equals(foods[i])) {
                 return i;
             }
         }
         return -1;
     }
+
+    // foods배열에서 맨 끝데이터를 삭제하는 함수
+    static void pop() {
+        foods = copy(-1);
+    }
+
+    static void remove(String deleteTarget) {
+        int index = indexOf(deleteTarget);
+        if (index == -1) return;
+
+        for (int i = index; i < foods.length - 1; i++) {
+            foods[i] = foods[i + 1];
+        }
+        pop();
+    }
+
 
 
     public static void main(String[] args) {
@@ -54,13 +74,16 @@ public class MethodQuiz01T {
 
         int index = indexOf("파스타");
         System.out.println("index = " + index);
-//
-//        int index2 = indexOf("라면땅");
-//        System.out.println("index2 = " + index2);
-//
-//        remove("치킨");
-//        printFoods();
-//
+
+        int index2 = indexOf("라면땅");
+        System.out.println("index2 = " + index2);
+
+        pop();
+        printFoods();
+
+        remove("치킨");
+        printFoods();
+
 //        boolean flag1 = include("파스타");
 //        System.out.println("flag1 = " + flag1);
 //
