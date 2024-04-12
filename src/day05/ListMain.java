d6a93099a62f49bdd4f803d5aa271f6b9df902c7
kkimjuseong <rofodzl1@naver.com>
2024-04-12 10:21:07 +0900

    # ListMain

--- src/day05/ListMain.java
+++ src/day05/ListMain.java
@@ -7,6 +7,7 @@ public class ListMain {
         StringList foods = new StringList();
         StringList userNames = new StringList();
 
+
         // 배열 내부 데이터 수 확인
         System.out.println(foods.size());
 
@@ -28,18 +29,30 @@ public class ListMain {
         foods.insert(1,"마라탕");
 
         // 배열 데이터 수정
-//        foods.set(0, "김치볶음밥"); // 0번 인덱스가 수정 되어야 함.
-//        userNames.set(1, "대길이"); // 1번 인덱스가 수정 되어야 함.
+        foods.set(0, "김치볶음밥"); // 0번 인덱스가 수정 되어야 함.
+        userNames.set(1, "대길이"); // 1번 인덱스가 수정 되어야 함.
 
         // 배열 데이터 전체 삭제
-//        foods.clear(); // foods배열 내부가 전체 삭제되고 size가 0이 되어야 함
+        foods.clear(); // foods배열 내부가 전체 삭제되고 size가 0이 되어야 함
 
         // 배열 데이터가 한개라도 있는지 확인
-//        boolean flag = foods.isEmpty(); // 배열이 비어 있으면 true, 아니면 false
+        boolean flag = foods.isEmpty(); // 배열이 비어 있으면 true, 아니면 false
 
 
         System.out.println("foods = " + foods.toString());
         System.out.println("userNames = " + userNames.toString());
+        System.out.println("foodsFlag: " + flag);
+
+
+
+        // 초기 배열에 생성자에 전달한 데이터들이 들어있어야 함.
+        StringList hobbies = new StringList("축구,", "영화감상", "음악감상");
+        System.out.println("hobbies = " + hobbies.toString());
+
+        hobbies.push("멍때리기");
+        // [축구, 영화감상, 음악감상]
+        System.out.println("hobbies = " + hobbies.toString());
+
 
     }
 }
