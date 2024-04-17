c7bcd6fa77ad4c55ed244177076f8815608677f0
kkimjuseong <rofodzl1@naver.com>
2024-04-17 12:26:27 +0900

    # 접근제한자 은행업무

---
+++ src/day07/encap/practice/BankAccount.java
@@ -0,0 +1,33 @@
+package day07.encap.practice;
+
+/**
+ * 은행 계좌 클래스 만들기
+ * 은행 계좌를 관리하는 BankAccount 클래스를 만드세요.
+ * 계좌 잔액(balance), 계좌 번호(accountNumber), 계좌 주인의 이름(accountHolder) 필드를 private로 선언하세요.
+ * 잔액 조회, 입금, 출금 메소드를 public으로 만들어 외부에서 접근할 수 있게 하세요.
+ * 출금 메소드는 잔액이 출금하려는 금액보다 클 때만 돈을 출금할 수 있도록 하세요.
+ */
+public class BankAccount {
+
+    private String accountNumber;
+    private String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolder, double balance){
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(int i) {
+        balance += i;
+    }
+    public void withdraw(int i) {
+        balance -= i;
+    }
+
+}
