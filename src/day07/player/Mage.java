dfaeae2779090e0499510c33130835d9c46fbab6
kkimjuseong <rofodzl1@naver.com>
2024-04-19 10:54:04 +0900

    # 다형성 썬더볼트예제

--- src/day07/player/Mage.java
+++ src/day07/player/Mage.java
@@ -13,9 +13,27 @@ public class Mage extends Player {
     }
 
     // 스킬
-    public void thunderbolt(Mage... target) {
+    public void thunderbolt(Player... target) {
+        System.out.printf("# %s님 썬더볼트 시전!!!\n", this.nickname);
 
+        // 대상에게 피해를 줌
+        for (Player player : target) {
+            if(player == this){
+                // 자기 자신은 피해를 입지 않음
+//                System.out.println("피해를 입지 않았습니다.");
+//                continue;
+            } else {
+                int damage = (int)(Math.random()*6 + 10);
+
+                // 대상에게 피해 주기
+                player.hp -= damage;
+
+                // 피해 정보 출력
+                System.out.println(player.nickname + " 님이 " + damage + " 의 피해를 입으셨습니다. (남은체력 : " + player.hp + ")");
+            }
+        }
     }
+
 
     public void showStatus(){
         super.showStatus();
