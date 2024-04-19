55d8e224e6f9abe8ca1ce2b919530cfd50a31644
kkimjuseong <rofodzl1@naver.com>
2024-04-19 10:35:16 +0900

    # 다형성 추가

--- src/day09/poly/car/Main.java
+++ src/day09/poly/car/Main.java
@@ -37,7 +37,31 @@ public class Main {
         Driver driver = new Driver();
         driver.drive(new Sonata());
 
+        Sonata mySonata = new Sonata();
+        mySonata.accelerate();
+        mySonata.joinSonataClub();
 
+        Car [] cars = new Car[3];
+        cars[0] = mySonata;
+
+        System.out.println("====================");
+
+        CarShop shop = new CarShop();
+
+        // 객체 타입에서 다운캐스팅의 전제조건 : 반드시!! 상속관계가 있어야하며
+        // 자식객체가 부모다입으로 업 캐스딩 된 것만 가능하다.
+        // 즉 , 부모객체를 자식타입으로 내릴 수는 없다.
+        Sonata car = (Sonata) shop.exportCar(3000);
+        car.accelerate();
+        car.joinSonataClub();
+
+        System.out.println("==========================");
+
+
+//        Sonata ccc = (Sonata) new Car();
+//        Sonata mmm = (Sonata) new Mustang();
+        Car ttt = new Tucson();
+        Tucson tts = (Tucson) ttt;
 
     }
 }
--- src/day09/poly/car/Sonata.java
+++ src/day09/poly/car/Sonata.java
@@ -7,4 +7,8 @@ public class Sonata extends Car{
         System.out.println("소나타가 달립니다");
     }
 
+    // 소나타 동호회에 가입하는 기능
+    public void joinSonataClub() {
+
+    }
 }
