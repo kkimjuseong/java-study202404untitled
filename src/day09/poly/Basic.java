d9c4ec5dba7b924997e164b878c93739726a997c
kkimjuseong <rofodzl1@naver.com>
2024-04-19 09:45:37 +0900

    # 다형성

---
+++ src/day09/poly/Basic.java
@@ -0,0 +1,29 @@
+package day09.poly;
+
+class A{}
+class B extends A {}
+class C extends A {}
+class D extends B {}
+class E extends C {}
+
+public class Basic {
+
+    void test (){
+
+        // 다형성 : 객체가 여러 타입을 사용할 수 있다.
+        // - 부모의 타입을 사용 가능.
+
+        A x1 = new A();
+        A x2 = new B();
+        A x3 = new C();
+        A x4 = new D();
+        A x5 = new E();
+
+        int a = 10;
+        double b = (double) a;
+
+        C c = new C();
+        A d = c;
+    }
+
+}
