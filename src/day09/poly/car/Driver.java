76093ffbfaadcaf929568ec2b2c048f40ea46291
kkimjuseong <rofodzl1@naver.com>
2024-04-19 14:15:33 +0900

    # 다형성

--- src/day09/poly/car/Driver.java
+++ src/day09/poly/car/Driver.java
@@ -3,6 +3,18 @@ package day09.poly.car;
 public class Driver {
 
     public void drive(Car car){
+
+        String carName = "";
+        if(car instanceof Sonata){
+            carName = "Sonata";
+        } else if (car instanceof Tucson) {
+            carName = "Tucson";
+        } else if (car instanceof Mustang) {
+            carName = "Mustang";
+        }
+
+        System.out.println(car instanceof Sonata);
+
         System.out.println("운전을 시작합니다.");
         car.accelerate();
     }
--- src/day09/poly/car/Main.java
+++ src/day09/poly/car/Main.java
@@ -63,5 +63,10 @@ public class Main {
         Car ttt = new Tucson();
         Tucson tts = (Tucson) ttt;
 
+
+        System.out.println("==========================");
+
+        driver.drive(new Tucson());
+
     }
 }
