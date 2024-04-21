6d7000e0e948ffac00deeb9ce554775a0e1b1cff
kkimjuseong <rofodzl1@naver.com>
2024-04-21 21:42:53 +0900

    # Book

--- src/day09/book/LibraryRepository.java
+++ src/day09/book/LibraryRepository.java
@@ -1,9 +1,11 @@
 package day09.book;
 
+// 역할: 도서관리 앱에서 사용하는 데이터들을 관리하는 객체
+// 관리할 데이터: 우리 회원이 누구?  우리는 어떤책들을 갖고 있는가?
 public class LibraryRepository {
 
-    private static BookUser user;
-    private static BookList bookList;
+    private static BookUser user; // 회원은 한명
+    private static BookList bookList;  // 책은 여러개
 
     // 정적 초기화자 : static필드 초기값 설정
     static {
@@ -17,21 +19,18 @@ public class LibraryRepository {
         bookList.push(new CartoonBook("이세계로 전이했으니 치트를 살려 마법검사가 되기로 했다", "Shinkoshoto", "대원씨아이", 12));
     }
 
+    // 저장소에 user를 저장
     public void saveBookUser(BookUser bookUser) {
         user = bookUser;
     }
 
-
+    // 책 정보가 들어 있는 배열을 리턴
     public Book[] getAllBooksInfo() {
         return bookList.getbArr();
     }
 
-
-    public BookUser getBookUser() {
-        return user;
-    }
-
     public Book[] searchBookList(String keyword) {
+
         // 검색어에 걸린 책들을 모아두는 리스트
         BookList filteredList = new BookList();
 
@@ -43,7 +42,13 @@ public class LibraryRepository {
         return filteredList.getbArr();
     }
 
+    /**
+     * 주어진 책 번호에 맞는 책이 대여 가능한지에 대한 상태상수를 리턴
+     * @param bookNum - 주어진 책 번호
+     * @return - 대여 가능 상태를 반환
+     */
     public RentStatus rentBook(int bookNum) {
+
         // 1. 책번호에 해당하는 책 정보 가져오기
         Book wishBook = bookList.get(bookNum - 1);
         // 2. 이 책을 대여할 수 있는지 검증
@@ -71,5 +76,9 @@ public class LibraryRepository {
         }
         return RentStatus.RENT_FAIL;
     }
-}
 
+    public BookUser getBookUser() {
+
+        return user;
+    }
+}
--- src/day09/book/LibraryView.java
+++ src/day09/book/LibraryView.java
@@ -1,40 +1,39 @@
 package day09.book;
 
+
 import Util.SimpleInput;
 
-import static Util.SimpleInput.input;
-
-// 역할 : 도서관리 프로그램에서 입출력을 담당하는 객체
-
+// 역할: 도서관리 프로그램에서 입출력을 담당하는 객체
 public class LibraryView {
 
-    // 회원 정보를 입력받는 기능
     private LibraryRepository repository;
-    SimpleInput input;
+    static SimpleInput si;
+
 
     public LibraryView() {
-        repository = new LibraryRepository();
-        input = new SimpleInput();
+        this.repository = new LibraryRepository();
+        si = new SimpleInput();
     }
 
+    // 회원 정보를 입력받는 기능
     public void makeNewBookUser() {
         System.out.println("\n# 회원 정보를 입력해주세요.");
-        String name = input("# 이름 : ");
-        int age = Integer.parseInt(input("# 나이 : "));
+        String name = si.input("# 이름: ");
+        int age = Integer.parseInt(si.input("# 나이: "));
         Gender gender = inputGender();
 
-        // 입력된 데이터 저장
+        // 입력된 데이터를 저장시켜야 함.
         repository.saveBookUser(new BookUser(name, age, gender, 0));
     }
 
-    // 성별을 제대로 입력 할때까지 무한히 반복
-
+    // 성별을 정확히 입력할때가지 무한히 입력받고
+    // 정확히 입력하면 해당 성별 문자를 리턴
     private Gender inputGender() {
         while (true) {
-            String gender = input("# 성별(M/F): ").toString();
-            if (gender.equals("M")) return Gender.MALE;
-            if (gender.equals("F")) return Gender.FEMALE;
-            System.out.println("\n# 성별을 잘못 입력하셨습니다.");
+            String gender = si.input("# 성별(M/F): ").toUpperCase();
+            if (gender.startsWith("M")) return Gender.MALE;
+            if (gender.startsWith("F")) return Gender.FEMALE;
+            System.out.println("\n# 성별을 잘못 입력했습니다.");
         }
     }
 
@@ -48,15 +47,15 @@ public class LibraryView {
         System.out.println("# 9. 프로그램 종료하기");
     }
 
-    // 코드 흐름을 캡슐화
+    // 코드의 흐름을 캡슐화
     public void start() {
         makeNewBookUser();
 
-        while(true) {
+        while (true) {
             showMainScreen();
-            String menuNem = input("- 메뉴 번호 : ");
+            String menuNum = si.input("- 메뉴 번호 : ");
 
-            switch (menuNem){
+            switch (menuNum) {
                 case "1":
                     displayUserInfo();
                     break;
@@ -76,27 +75,32 @@ public class LibraryView {
                     System.out.println("# 올바른 메뉴 번호를 입력하세요!");
             }
         }
-
     }
 
-    private void rentBook() {
-        displayAllBooks();
-        String bookNum = input("- 대여할 도서 번호 입력: ");
-        // 저장소에다가 대여가능한지 여부 검증
-        RentStatus status = repository.rentBook(Integer.parseInt(bookNum));
+    private void displayUserInfo() {
+        BookUser user = repository.getBookUser();
+        System.out.println("\n******** 회원님 정보 ********");
+        System.out.println("# 회원명: " + user.getName());
+        System.out.println("# 나이: " + user.getAge());
+        System.out.println("# 성별: " + user.getGenderToString());
+        System.out.println("# 쿠폰개수: " + user.getCouponCount());
+    }
 
-        if (status == RentStatus.RENT_SUCCESS_WITH_COUPON) {
-            System.out.println("# 성공적으로 요리책이 쿠폰발급과 함께 대여되었습니다.");
-        } else if (status == RentStatus.RENT_SUCCESS) {
-            System.out.println("# 도서가 성공적으로 대여되었습니다.");
-        } else {
-            System.out.println("# 도서 대여에 실패했습니다.");
+    // 전체 도서 정보를 출력
+    private void displayAllBooks() {
+        System.out.println("\n=============== 전체 도서 목록 ================");
+        Book[] informationList = repository.getAllBooksInfo();
+
+        for (int i = 0; i < informationList.length; i++) {
+            Book book = informationList[i];
+            System.out.printf("%d. %s\n", i+1, book.info());
         }
-
     }
 
+    // 책 제목으로 검색어포함된 책 내용 출력하기
     private void searchBooksByTitle() {
-        String keyword = input("# 검색어 : ");
+        String keyword = si.input("# 검색어: ");
+
         Book[] searchBooks = repository.searchBookList(keyword);
 
         if (searchBooks.length > 0) {
@@ -109,22 +113,20 @@ public class LibraryView {
         }
     }
 
-    private void displayAllBooks() {
-        BookUser user = repository.getBookUser();
-        System.out.println("\n******** 회원님 정보 ********");
-        System.out.println("# 회원명: " + user.getName());
-        System.out.println("# 나이: " + user.getAge());
-        System.out.println("# 성별: " + user.getGenderToString());
-        System.out.println("# 쿠폰개수: " + user.getCouponCount());
-    }
+    private void rentBook() {
+        displayAllBooks();
+        String bookNum = si.input("- 대여할 도서 번호 입력: ");
+        // 저장소에다가 대여가능한지 여부 검증
+        RentStatus status = repository.rentBook(Integer.parseInt(bookNum));
 
-    private void displayUserInfo() {
-        System.out.println("\n=============== 전체 도서 목록 ================");
-        Book[] informationList = repository.getAllBooksInfo();
-
-        for (int i = 0; i < informationList.length; i++) {
-            Book book = informationList[i];
-            System.out.printf("%d. %$s\n", i + 1, book.getTitle());
+        if (status == RentStatus.RENT_SUCCESS_WITH_COUPON) {
+            System.out.println("# 성공적으로 요리책이 쿠폰발급과 함께 대여되었습니다.");
+        } else if (status == RentStatus.RENT_SUCCESS) {
+            System.out.println("# 도서가 성공적으로 대여되었습니다.");
+        } else {
+            System.out.println("# 도서 대여에 실패했습니다.");
         }
     }
+
+
 }
