e65f283ada66a463284717b528e0d17ff2131261
kkimjuseong <rofodzl1@naver.com>
2024-04-22 12:22:56 +0900

    # interface Quiz

--- src/day10/inter/quiz_interface_/MediaPlayer.java
+++ src/day10/inter/quiz_interface_/MediaPlayer.java
@@ -10,7 +10,10 @@ public class MediaPlayer{
         // 주어진 media 를 mediaList 배열에 추가
         MediaPlayable[] newMediaList = new MediaPlayable[mediaList.length + 1];
 
-        // newMediaList = Arrays.copyOf(mediaList, mediaList.length);
+        // 위 newMediaList 를 만들지 않고 아래처럼 만들면 for 반복문까지 해결가능
+        // MediaPlayable[] newMediaList = Arrays.copyOf(mediaList, mediaList.length + 1); // 배열 복사
+
+
         // 밑에 for문을 안돌려도 배열복사가 가능함
 
         for (int i = 0; i < mediaList.length; i++) {
