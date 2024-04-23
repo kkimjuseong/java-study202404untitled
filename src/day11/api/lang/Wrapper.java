16bc208c6a4f4a1ec5849bd91ca8e8c0c10cbb08
kkimjuseong <rofodzl1@naver.com>
2024-04-23 10:42:08 +0900

    # 자바 표준 API - Wrapper

---
+++ src/day11/api/lang/Wrapper.java
@@ -0,0 +1,25 @@
+package day11.api.lang;
+
+public class Wrapper {
+
+    // 기본값 차이
+    private int number1;        // 기본값 0
+    protected Integer number2;  // 기본값 null
+
+    Wrapper (){
+        this.number2 = 0;
+    }
+
+    public static void main(String[] args) {
+
+        // byte short int long boolean float double char
+        // primitive type (기본 타입, 기초 타입, 원시 타입)
+
+        // Wrapper type
+        // Byte Short Integer Long Boolean Float Double Character
+
+        Integer a = 10;
+        Double b = 3.3;
+
+    }
+}
